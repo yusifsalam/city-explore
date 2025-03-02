56fdcaafa94152d89fa9c2ee96f732ce1e4419da
Yusif Salam-zade <42574232+yusifsalam@users.noreply.github.com>
2025-03-02 21:59:31 +0200

    handle missing location authorization

--- City Explore/CityExploreApp.swift
+++ City Explore/CityExploreApp.swift
@@ -2,9 +2,32 @@ import SwiftUI
 
 @main
 struct CityExploreApp: App {
+    @Environment(\.openURL) private var openURL
+    @State private var locationManager = LocationManager()
     var body: some Scene {
         WindowGroup {
-            ContentView()
+            if locationManager.isAuthorized {
+                ContentView()
+            } else {
+                ContentUnavailableView {
+                    Label("Location Services Not Enabled", systemImage: "exclamationmark.triangle")
+                    
+                    Button("Open App Settings") {
+                        if let url = URL(string: UIApplication.openSettingsURLString) {
+                            openURL(url)
+                        }
+                    }
+                    .padding()
+                    .background(Color.blue)
+                    .foregroundColor(.white)
+                    .cornerRadius(8)
+                }
+                
+            }
+            
         }
+        .environment(locationManager)
     }
+    
 }
+
